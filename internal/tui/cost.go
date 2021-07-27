9ae354e30ad10e9bd2e31237532ff2f5f1401c25
droctothorpe <mythicalsunlight@gmail.com>
2021-07-27 19:48:36 -0400

    Finetune cost calculation

--- internal/tui/cost.go
+++ internal/tui/cost.go
@@ -9,9 +9,9 @@ func calculateCost(data *Data) {
 		total := data.getCost()
 		// FLC explained:
 		// https://smallbusiness.chron.com/calculate-fully-burdened-labor-costs-33072.html
-		// TODO: Make FLC configuration via the config file.
+		// TODO: Make FLC configurable via the config file.
 		fullyLoadedCostMultiplier := float32(1.75)
-		cps := float32(count) * fullyLoadedCostMultiplier * float32(150000) / 7488000
+		cps := float32(count) * fullyLoadedCostMultiplier * float32(150000) / 7200000 // 50 (weeks) * 40 (hours) * 60 (minutes) * 60 (seconds)
 		total += cps
 		data.setCost(total)
 		time.Sleep(refreshInterval)
